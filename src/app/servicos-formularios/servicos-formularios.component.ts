5ae9b580d0f0f9d05463c65e5fdaf35e21c2d899
Henrique Wilson <me@henriquewilson.com.br>
2021-03-02 08:58:22 -0300

    modal

--- src/app/servicos-formularios/servicos-formularios.component.ts
+++ src/app/servicos-formularios/servicos-formularios.component.ts
@@ -1,23 +1,53 @@
-import { Component, OnInit } from '@angular/core';
-import { Location } from '@angular/common';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
+import {Location} from '@angular/common';
+import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'app-servicos-formularios',
   templateUrl: './servicos-formularios.component.html',
   styleUrls: ['./servicos-formularios.component.scss']
 })
-export class ServicosFormulariosComponent implements OnInit {
+export class ServicosFormulariosComponent implements OnInit, AfterViewInit {
 
+  showForm1 = false;
+  showForm2 = false;
+  showForm3 = false;
+  showForm4 = false;
+  showForm5 = false;
+  showForm6 = false;
+  showForm7 = false;
+  showForm8 = false;
+  showForm9 = false;
 
-  constructor(
-    private location: Location,
-  ) { }
+  imgBoleto = 1;
+
+  constructor(private location: Location, private modalService: NgbModal) {
+  }
 
   ngOnInit() {
   }
 
+  ngAfterViewInit() {
+    try {
+      window.scrollTo({left: 0, top: 0, behavior: 'smooth'});
+    } catch (e) {
+      window.scrollTo(0, 0);
+    }
+  }
+
+
   mToggle(event) {
 
+  }
+
+  open(content) {
+    this.modalService.open(content, {
+      ariaLabelledBy: 'modal-basic-title',
+      size: 'lg',
+      centered: true
+    }).result.then((result) => {
+    }, (reason) => {
+    });
   }
 
 
