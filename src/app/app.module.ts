b23f24313c59b2fcd07e3a5e7403eff1657d77a5
Henrique Wilson <me@henriquewilson.com.br>
2023-07-30 22:27:41 -0300

    LGPD

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {LOCALE_ID, NgModule} from '@angular/core';
-
 import {AppComponent} from './app.component';
 import {HeaderComponent} from './header/header.component';
 import {ImoveisComponent} from './imoveis/imoveis.component';
@@ -16,7 +15,7 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatSliderModule} from '@angular/material/slider'
+import {MatSliderModule} from '@angular/material/slider';
 import {Ng5SliderModule} from 'ng5-slider';
 import {WPService} from './core/services/w-p.service';
 import {SobreComponent} from './sobre/sobre.component';
@@ -26,32 +25,33 @@ import {ComponentFixtureAutoDetect} from '@angular/core/testing';
 import {NgxUiLoaderConfig, NgxUiLoaderModule} from 'ngx-ui-loader';
 import {CommonModule, registerLocaleData} from '@angular/common';
 import localePt from '@angular/common/locales/pt';
-import {environment} from "../environments/environment";
-import {AllImoveis} from "./core/services/all-imoveis.service";
+import {environment} from '../environments/environment';
+import {AllImoveis} from './core/services/all-imoveis.service';
 import {FabComponent} from './fab/fab.component';
 import {MomentModule} from 'ngx-moment';
-import {NgImageSliderModule} from "ng-image-slider";
-import {LancamentoComponent} from "./imoveis/lancamento/lancamento.component";
-import {CarouselModule} from "ngx-owl-carousel-o";
-import {NgBrazil} from "ng-brazil";
-import {TextMaskModule} from "angular2-text-mask";
+import {NgImageSliderModule} from 'ng-image-slider';
+import {LancamentoComponent} from './imoveis/lancamento/lancamento.component';
+import {CarouselModule} from 'ngx-owl-carousel-o';
+import {NgBrazil} from 'ng-brazil';
+import {TextMaskModule} from 'angular2-text-mask';
 import {BlogComponent} from './blog/blog.component';
 import {BlogDetailComponent} from './blog/blog-detail/blog-detail.component';
 import {ServicosFormulariosComponent} from './servicos-formularios/servicos-formularios.component';
 import {FormPessoaFisicaComponent} from './servicos-formularios/form-pessoa-fisica/form-pessoa-fisica.component';
 import {LancamentosComponent} from './imoveis/lancamentos/lancamentos.component';
-import {initializeApp, provideFirebaseApp} from "@angular/fire/app";
-import {getFirestore, provideFirestore} from "@angular/fire/firestore";
-import {AngularFireModule} from "@angular/fire/compat";
-import {AngularFirestoreModule} from "@angular/fire/compat/firestore";
-import {GoogleMapsModule} from "@angular/google-maps";
+import {initializeApp, provideFirebaseApp} from '@angular/fire/app';
+import {getFirestore, provideFirestore} from '@angular/fire/firestore';
+import {AngularFireModule} from '@angular/fire/compat';
+import {AngularFirestoreModule} from '@angular/fire/compat/firestore';
+import {GoogleMapsModule} from '@angular/google-maps';
 import {NextToastComponent} from './core/components/next-toast/next-toast.component';
 import {CustomSearchComponent} from './core/components/custom-search/custom-search.component';
-import {MatFormFieldModule} from "@angular/material/form-field";
-import {MatSelectModule} from "@angular/material/select";
-import {MatInputModule} from "@angular/material/input";
-import {MatOptionModule} from "@angular/material/core";
-import {NgImageFullscreenViewModule} from "ng-image-fullscreen-view";
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatOptionModule} from '@angular/material/core';
+import {NgImageFullscreenViewModule} from 'ng-image-fullscreen-view';
+import {NgcCookieConsentModule, NgcCookieConsentConfig} from 'ngx-cookieconsent';
 
 registerLocaleData(localePt);
 
@@ -80,6 +80,30 @@ const ngxUiLoaderConfig: NgxUiLoaderConfig = {
   text: '',
   textColor: '#FFFFFF',
   textPosition: 'center-center',
+};
+
+const cookieConfig: NgcCookieConsentConfig = {
+  cookie: {
+    domain: 'nextsim.com.br'
+  },
+  palette: {
+    popup: {
+      background: '#575656'
+    },
+    button: {
+      background: '#fe6801'
+    }
+  },
+  content: {
+    message: 'Usamos cookies para personalizar conteúdos e melhorar sua experiência. Ao navegar nesse site, você concorda com a nossa ',
+    dismiss: 'Entendi',
+    deny: 'Recusar cookies',
+    link: 'Política de Cookies',
+    href: 'https://firebasestorage.googleapis.com/v0/b/next-jsonserver.appspot.com/o/lgpd%2FPoli%CC%81tica%20de%20Privacidade.pdf?alt=media&token=a5f8037d-fa3f-4f32-a0e5-d1f24ecc1a87',
+    policy: 'Política de Cookies'
+  },
+  theme: 'block',
+  type: 'info',
 };
 
 @NgModule({
@@ -120,6 +144,7 @@ const ngxUiLoaderConfig: NgxUiLoaderConfig = {
     NgBrazil,
     TextMaskModule,
     NgImageSliderModule,
+    NgcCookieConsentModule.forRoot(cookieConfig),
     ToastrModule.forRoot(),
     RouterModule.forRoot(ROUTES, {relativeLinkResolution: 'legacy'}),
     FormsModule,
