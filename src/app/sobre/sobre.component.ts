ef54fd698537b83a2bcce9b58c68f10de9b3c40b
Henrique Wilson <me@henriquewilson.com.br>
2023-08-14 12:55:21 -0300

    fix page sobre

--- src/app/sobre/sobre.component.ts
+++ src/app/sobre/sobre.component.ts
@@ -1,5 +1,9 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {WPService} from "../core/services/w-p.service";
+import {Observable, of} from "rxjs";
+import {environment} from "../../environments/environment";
+import {catchError, map} from "rxjs/operators";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-sobre',
@@ -11,12 +15,19 @@ export class SobreComponent implements OnInit {
   latitude = -22.902578;
   longitude = -47.041508;
 
+  apiLoaded: Observable<boolean>;
+
   historias: Historia[] = [];
 
-  constructor(private lancamentoService: WPService) { }
+  constructor(private lancamentoService: WPService, private httpClient: HttpClient) { }
 
   ngOnInit() {
 
+    this.apiLoaded = this.httpClient.jsonp(`https://maps.googleapis.com/maps/api/js?key=${environment.maps.key}`, 'callback')
+      .pipe(
+        map(() => true),
+        catchError(() => of(false)),
+      );
     try {
       window.scrollTo({left: 0, top: 0, behavior: 'smooth'});
     } catch (e) {
